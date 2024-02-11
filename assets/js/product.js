c09e071e6b4c7561fe9b90fb7c926481d87f9210
NadaNaguib <nada.naguib,asfour@gmail.com>
2024-02-11 17:02:13 +0200

    search product

--- assets/js/product.js
+++ assets/js/product.js
@@ -28,7 +28,7 @@ async function getProductByCategory(category) {
 async function searchProductName(productName) {
     const response = await fetch(`https://dummyjson.com/products/search?q=${productName}`);
     let data = await response.json();
-    products = data.products;
+    products = data.products.filter(p => p.title.toLowerCase().includes(productName.toLowerCase()));
     createProductsCard();
 }
 function filterByCategory(category){
@@ -43,47 +43,44 @@ function filterByCategory(category){
 function createProductsCard() {
     console.log(products);
     let cardsContainer = document.querySelector(".cards-container");
-    let cards = [];
+    let cards = "";
     for(let i = 0; i < products.length; i++){
-        let card = document.createElement("div");
-        let image = document.createElement("img");
-        let title = document.createElement("h3");
-        let cardFooter = document.createElement("div");
-        let rateDiv = document.createElement("div");
-        let price = document.createElement("span");
-        image.src = products[i].images[0];
-        title.innerHTML = products[i].title;
-        price.innerHTML = "$ " + products[i].price;
-        cardFooter.classList.add("card-footer");
-        rateDiv.classList.add("rate");
-        card.classList.add("card");
-        for(let j = 0; j < 5; j++){
-            let starSpan = document.createElement("span");
-            let starIcon = document.createElement("i");
-            if((j + 1) < products[i].rating)
-            {
-                starIcon.setAttribute("class","fas fa-star");
-            }
-            else{
-                starIcon.setAttribute("class","far fa-star");
-            }
-            starSpan.appendChild(starIcon);
-            rateDiv.appendChild(starSpan);
+        let rate = "";
+        for(let j = 1; j <= 5; j++){
+            rate += `
+                <span><i class="${j < products[i].rating ? "fas fa-star" : "far fa-star"}" aria-hidden="true"></i></span>
+            `
         }
-        cardFooter.append(rateDiv, price);
-        card.append(image, title, cardFooter);
-        cards.push(card);
+        cards += `
+            <a class="card" href="">
+                <img src="${products[i].images[0]}">
+                <h3>${products[i].title}</h3>
+                <div class="card-footer">
+                    <div class="rate">${rate}</div>
+                    <span>$ ${products[i].price}</span>
+                </div>
+            </a>
+        `;
     }
-    cardsContainer.replaceChildren(...cards);
+    cardsContainer.innerHTML = cards;
 }
 function createCategoriesList() {
     console.log(categories);
     let select = document.querySelector("#categories");
+    let options = "";
     for(let i = 0; i<categories.length; i++){
-        let option = document.createElement("option");
-        option.innerHTML = categories[i];
-        option.value = categories[i];
-        select.append(option);
+        options += `
+            <option value="${categories[i]}">${categories[i]}</option>
+        `;
+    }
+    select.innerHTML += options;
+}
+function search(){
+    var searchInput = document.querySelector("#search");
+    if(searchInput.value){
+        searchProductName(searchInput.value);
+        var productContainer = document.querySelector("#products");
+        window.scrollTo(0,productContainer.offsetTop);
     }
 }
 getAllCategories();
