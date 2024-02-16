2a98010292ff8676174284bdd08dfc792de4d28b
omarwagdy10 <omarwagdy122@gmail.com>
2024-02-16 20:31:06 +0200

    small change in slider

--- assets/js/product.js
+++ assets/js/product.js
@@ -31,150 +31,145 @@ async function searchProductName(productName) {
     products = data.products.filter(p => p.title.toLowerCase().includes(productName.toLowerCase()));
     createProductsCard();
 }
-function filterByCategory(category) {
+function filterByCategory(category){
     debugger
-    if (category) {
+    if(category){
         getProductByCategory(category);
     }
-    else {
+    else{
         getAllPrducts();
     }
 }
+
 function createProductsCard() {
     console.log(products);
     let cardsContainer = document.querySelector(".cards-container");
     let cards = "";
     let owlTheme = document.querySelector(".carousel-container")
-    for (let i = 0; i < products.length; i++) {
+    for(let i = 0; i < products.length; i++){
         let rate = "";
-
-        for (let j = 1; j <= 5; j++) {
+    
+        for(let j = 1; j <= 5; j++){
             rate += `
                 <span><i class="${j < products[i].rating ? "fas fa-star" : "far fa-star"}" aria-hidden="true"></i></span>
             `
         }
         cards += `
-        <div class="card">
-            <a onclick="saveId(${products[i].id})" href="Single-Prod.html">
-                <img src="${products[i].images[0]}">
-                <h3>${products[i].title}</h3>
-                <div class="card-footer">
-                <div class="rate">${rate}</div>
-                <span>$ ${products[i].price}</span>
-                </div>
+            <a onclick="saveId(${products[i].id})" class="card" href="Single-Prod.html">
+            <img src="${products[i].images[0]}">
+            <h3>${products[i].title}</h3>
+            <div class="card-footer">
+            <div class="rate">${rate}</div>
+            <span>$ ${products[i].price}</span>
+            </div>
+            <button class="cart-icon" onclick="">Add to cart</button>
             </a>
-            <button class="cart-icon" onclick="${token ? `addToCart(${products[i].id})` : `alert('please sign in!');`}">Add to cart</button> 
-
-        </div>
-            
         `
-
-            ;
+        
+        ;
     }
-
+    
     cardsContainer.innerHTML = cards;
 
 
-    carouselData = `
+            carouselData = `
             <div class="owl-carousel owl-theme">
-            <div class="item">            
-            <img src=${products[1].images[0]}>
 
             
-            <div class ="carsule-cont">
-            <h4> prod title:${products[1].title}</h4>
-            <h4> prod category: ${products[1].category}</h4>
-            <h4> prod rating:<span><i class="fa-solid fa-star star"></i></span>  ${products[1].rating} /5 </h4>
+            <div class="item">
+            
+            <img src=${products[1].images[0]}>
+
+            <div class="carsule-cont">
+            
+            <h4> Product Title : ${products[1].title}</h4>
+            <h4> Product Category : ${products[1].category}</h4>
+            
             </div>
 
-
             </div>
 
-
-       
-            <div class="item">            
+            <div class="item">
+            
             <img src=${products[2].images[0]}>
 
-
-            <div class ="carsule-cont">
-            <h4> prod title:${products[2].title}</h4>
-            <h4> prod category: ${products[2].category}</h4>
-            <h4> prod rating:<span><i class="fa-solid fa-star star"></i></span>  ${products[2].rating} /5 </h4>
+            <div class="carsule-cont">
+            
+            <h4> Product Title : ${products[2].title}</h4>
+            <h4> Product Category : ${products[2].category}</h4>
+            
             </div>
 
-
             </div>
 
-
-       
-            <div class="item">            
+            <div class="item">
+            
             <img src=${products[3].images[0]}>
 
-
-            <div class ="carsule-cont">
-            <h4> prod title:${products[3].title}</h4>
-            <h4> prod category: ${products[3].category}</h4>
-            <h4> prod rating:<span><i class="fa-solid fa-star star"></i></span>  ${products[3].rating} /5 </h4>
+            <div class="carsule-cont">
+            
+            <h4> Product Title : ${products[3].title}</h4>
+            <h4> Product Category : ${products[3].category}</h4>
+            
             </div>
 
-
             </div>
 
-
-       
-            <div class="item">            
+            <div class="item">
+            
             <img src=${products[4].images[0]}>
 
-
-            <div class ="carsule-cont">
-            <h4> prod title:${products[4].title}</h4>
-            <h4> prod category: ${products[4].category}</h4>
-            <h4> prod rating:<span><i class="fa-solid fa-star star"></i></span>  ${products[4].rating} /5 </h4>
+            <div class="carsule-cont">
+            
+            <h4> Product Title : ${products[4].title}</h4>
+            <h4> Product Category : ${products[4].category}</h4>
+            
             </div>
 
-
             </div>
 
-
-       
-            <div class="item">            
+            <div class="item">
+            
             <img src=${products[5].images[0]}>
 
+            <div class="carsule-cont">
+            
+            <h4> Product Title : ${products[5].title}</h4>
+            <h4> Product Category : ${products[5].category}</h4>
+            
+            </div>
 
-            <div class ="carsule-cont">
-            <h4> prod title:${products[5].title}</h4>
-            <h4> prod category: ${products[5].category}</h4>
-            <h4> prod rating:<span><i class="fa-solid fa-star star"></i></span>  ${products[5].rating} /5 </h4>
             </div>
 
 
-            </div>
 
 
-       
+
+           
+        
             </div>
         
             `
-
-    owlTheme.innerHTML = carouselData
-
-
-
+        
+        owlTheme.innerHTML = carouselData
+    
+    
+            
     $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 35,
-        nav: false,
-        autplay: true,
-        autplayTimeout: 1000,
-        responsive: {
-            0: {
-                items: 1
+        loop:true,
+        margin:35,
+        nav:false,
+        autplay:true,
+        autplayTimeout:1000,
+        responsive:{
+            0:{
+                items:1
             },
-            600: {
-                items: 3
+            600:{
+                items:3
             },
-            1000: {
-                items: 5
+            1000:{
+                items:5
             }
         }
     })
@@ -184,35 +179,36 @@ function createProductsCard() {
 
 
 
+
 function createCategoriesList() {
     console.log(categories);
     let select = document.querySelector("#categories");
     let options = "";
-    for (let i = 0; i < categories.length; i++) {
+    for(let i = 0; i<categories.length; i++){
         options += `
             <option value="${categories[i]}">${categories[i]}</option>
         `;
     }
     select.innerHTML += options;
 }
-function search() {
+function search(){
     var searchInput = document.querySelector("#search");
-    if (searchInput.value) {
+    if(searchInput.value){
         searchProductName(searchInput.value);
         var productContainer = document.querySelector("#products");
-        window.scrollTo(0, productContainer.offsetTop);
+        window.scrollTo(0,productContainer.offsetTop);
     }
 }
 
-function saveId(id) {
-    localStorage.setItem("ProducID", JSON.stringify(id))
+function saveId(id){
+    localStorage.setItem("ProducID",JSON.stringify(id))
 
 }
 
 
 
 
-
+    
 
 getAllCategories();
 getAllPrducts();
